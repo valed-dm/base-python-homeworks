0cafd349433278f9093e46988b5c06489123fef1
valed-dm <dmvaled@gmail.com>
2023-01-31 18:09:09 +0300

    func power_numbers done

--- homework_01/main.py
+++ homework_01/main.py
@@ -4,13 +4,14 @@
 """
 
 
-def power_numbers():
+def power_numbers(*args):
     """
     функция, которая принимает N целых чисел,
     и возвращает список квадратов этих чисел
     >>> power_numbers(1, 2, 5, 7)
     <<< [1, 4, 25, 49]
     """
+    return [num ** 2 for num in args]
 
 
 # filter types
