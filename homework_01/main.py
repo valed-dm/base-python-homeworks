bad9d3076d677dee6c155531b73370262463b5eb
valed-dm <dmvaled@gmail.com>
2023-01-31 18:25:54 +0300

    func is_prime added

--- homework_01/main.py
+++ homework_01/main.py
@@ -20,6 +20,17 @@ EVEN = "even"
 PRIME = "prime"
 
 
+def is_prime(n):
+    if n <= 1:
+        return False
+
+    for i in range(2, n):
+        if n % i == 0:
+            return False
+
+    return True
+
+
 def filter_numbers():
     """
     функция, которая на вход принимает список из целых чисел,
