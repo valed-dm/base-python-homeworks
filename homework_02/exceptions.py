241236ce9e5dbcea4e5878004f65ea5559f7f6e2
valed-dm <dmvaled@gmail.com>
2023-02-04 11:27:08 +0300

    fuel, cargo exc. declared

--- homework_02/exceptions.py
+++ homework_02/exceptions.py
@@ -4,3 +4,18 @@
 - NotEnoughFuel
 - CargoOverload
 """
+
+
+class LowFuelError(Exception):
+    """Raised when the fuel level is low"""
+    pass
+
+
+class NotEnoughFuel(Exception):
+    """Raised when the fuel level is insufficient"""
+    pass
+
+
+class CargoOverload(Exception):
+    """Raised when the cargo exceeds capacity"""
+    pass
