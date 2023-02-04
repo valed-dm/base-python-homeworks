b1b84fc88b2f7333926cfc28770219a1558540de
valed-dm <dmvaled@gmail.com>
2023-02-04 15:21:16 +0300

    class Car set up

--- homework_02/car.py
+++ homework_02/car.py
@@ -1,3 +1,14 @@
 """
 создайте класс `Car`, наследник `Vehicle`
 """
+from homework_02.base import Vehicle
+from homework_02.engine import Engine
+
+
+class Car(Vehicle):
+    def __init__(self, *args):
+        super().__init__(*args)
+        self.engine = None
+
+    def set_engine(self, engine=Engine()):
+        self.engine = engine
