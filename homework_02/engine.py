82410a6973c8f8bd4b768d15b16ef9b96dfe8537
valed-dm <dmvaled@gmail.com>
2023-02-09 21:52:34 +0300

    class Engine refactored as dataclass

--- homework_02/engine.py
+++ homework_02/engine.py
@@ -6,6 +6,5 @@ from dataclasses import dataclass
 
 @dataclass
 class Engine:
-    def __init__(self, volume=None, pistons=None):
-        self.volume = volume
-        self.pistons = pistons
+    volume: int
+    pistons: int
