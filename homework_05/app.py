3f2bab6acd603060110ff95e7772f0c0da8da081
valed-dm <dmvaled@gmail.com>
2023-03-05 11:23:03 +0300

    app.py main file for FlaskApp created

--- homework_05/app.py
+++ homework_05/app.py
@@ -10,3 +10,16 @@
 в базовый шаблон добавьте навигационную панель nav (https://getbootstrap.com/docs/5.0/components/navbar/)
 в навигационную панель добавьте ссылки на главную страницу / и на страницу /about/ при помощи url_for
 """
+from flask import Flask
+from flask import render_template
+
+app = Flask(__name__, template_folder='templates', static_folder='static')
+
+
+@app.get("/")
+def index_view():
+    return render_template("index.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
