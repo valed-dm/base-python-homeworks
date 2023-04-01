a0fb49f41f252c5023eb645e4f772a46c8cce927
valed-dm <dmvaled@gmail.com>
2023-04-01 16:27:39 +0300

    select, filter logic moved to db level

--- homework_06/app/database/crud/add_book.py
+++ homework_06/app/database/crud/add_book.py
@@ -1,5 +1,7 @@
 from flask import flash, redirect, url_for
 from sqlalchemy import exists
+from sqlalchemy import func
+from sqlalchemy import select
 
 from database import db
 from gbooks.helpers import authors_string
@@ -8,9 +10,9 @@ from ..models import Author, Book, Category, Image
 
 def add_book(book, rubric, remarks):
     b = book
-    book_authors = [author.strip() for author in b.authors]
-    book_categories = [category.strip() for category in b.categories]
-    authors = authors_string(book_authors)
+    book_authors = [author.strip().lower() for author in b.authors]
+    book_categories = [category.strip().lower() for category in b.categories]
+    authors_str = authors_string(book_authors)
     id_exists = db.session.query(
         exists().
         where(Book.google_book_id == b.google_book_id)). \
@@ -19,7 +21,7 @@ def add_book(book, rubric, remarks):
     # prepares book data to be filled in books table
     if not id_exists:
         book_to_library = Book(
-            authors_str=authors,
+            authors_str=authors_str,
             title=b.title,
             description=b.description,
             date=b.date,
@@ -31,36 +33,28 @@ def add_book(book, rubric, remarks):
         )
 
         # fills in the authors table
-        db_authors = db.session.query(Author).all()
-        db_authors = [item.name.lower() for item in db_authors]
+        authors_lower = db.session.scalars(select(func.lower(Author.name))).all()
         for author in book_authors:
-            if author.lower() not in db_authors:
-                author_for_book = Author(name=author)
+            if author not in authors_lower:
+                author_for_book = Author(name=author.title())
                 db.session.add(author_for_book)
 
         # fills in the category table
-        db_categories = db.session.query(Category).all()
-        db_categories = [item.name.lower() for item in db_categories]
+        categories_lower = db.session.scalars(select(func.lower(Category.name))).all()
         for category in book_categories:
-            if category.lower() not in db_categories:
-                category_for_book = Category(name=category)
+            if category not in categories_lower:
+                category_for_book = Category(name=category.title())
                 db.session.add(category_for_book)
 
         # fills in the book-author association table
-        db_authors = db.session.query(Author).all()
-        db_authors = [
-            author for author in db_authors if author.name in book_authors
-        ]
-        for author in db_authors:
-            book_to_library.authors.append(author)
+        book_auth = db.session.query(Author).filter(func.lower(Author.name).in_(book_authors))
+        for auth in book_auth:
+            book_to_library.authors.append(auth)
 
         # fills in the book-category association table
-        db_categories = db.session.query(Category).all()
-        db_categories = [
-            category for category in db_categories if category.name in book_categories
-        ]
-        for category in db_categories:
-            book_to_library.categories.append(category)
+        book_cat = db.session.query(Category).filter(func.lower(Category.name).in_(book_categories))
+        for cat in book_cat:
+            book_to_library.categories.append(cat)
 
         # sends all new data to all 6 tables in "gbooks" db at once
         db.session.add(book_to_library)
