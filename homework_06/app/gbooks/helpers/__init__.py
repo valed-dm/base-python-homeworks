8123a1f4bb4f6f7225e6ee81a5e18999b9c76cf0
valed-dm <dmvaled@gmail.com>
2023-04-02 07:17:42 +0300

    unused str_to_class.py deleted forbidden eval function was used inside str_to_class.py

--- homework_06/app/gbooks/helpers/__init__.py
+++ homework_06/app/gbooks/helpers/__init__.py
@@ -1,3 +1,2 @@
 from .authors_string import authors_string
 from .sorting import sorting_results
-from .str_to_class import str_to_class
--- homework_06/app/gbooks/helpers/str_to_class.py
+++
@@ -1,8 +0,0 @@
-from ..schemas import Book
-
-
-def str_to_class(s: str) -> Book:
-    if isinstance(s, str):
-        book = eval(s)
-
-        return book
