2a19e2751c74d886bcefad02910250ba7285360e
valed-dm <dmvaled@gmail.com>
2023-04-13 08:46:00 +0300

    Google Books API interaction files

---
+++ homework_07/gbooks/api/api/fetch_uri.py
@@ -0,0 +1,20 @@
+import environ
+
+# Initialise environment variables
+env = environ.Env()
+environ.Env.read_env()
+
+api_key = env.str("GOOGLE_API_KEY")
+
+
+def fetch_uri_create(title: str = "", res_qty: str = "10", start_point: str = "0"):
+    api_uri = "https://www.googleapis.com/books/v1/volumes?q=" \
+              + title \
+              + "&key=" \
+              + str(api_key) \
+              + "&maxResults=" \
+              + res_qty + \
+              "&startIndex=" \
+              + start_point
+
+    return api_uri
