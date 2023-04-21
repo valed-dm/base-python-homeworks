ec0836d9072eb347960d599638208866072e7d1c
valed-dm <dmvaled@gmail.com>
2023-04-21 08:18:53 +0300

    LibraryListView finally refactored func-based transformation to class-based view done; BookDetailView altered to match common library templates

--- gbooks/library/views.py
+++ gbooks/library/views.py
@@ -9,42 +9,40 @@ from library.helpers import get_args_library
 from library.helpers import info_sort_library
 from library.helpers import library_to_view
 from library.helpers import sort_books
-from library.schemas import ArgsLibrary
+from library.schemas import ArgsLibrary, Library
 from .models import Book as DB_Book
 
 
 class LibraryListView(ListView):
     model = DB_Book
     template_name = "library/library_books.html"
+    context_object_name = "library"
 
     def get(self, request, *args, **kwargs):
         if "del" in request.GET:
             delete_book(request)
         return super().get(self, *args, *kwargs)
 
-    # POST requests are allowed
-    def post(self, *args, **kwargs):
-        return super().get(self, *args, **kwargs)
+    def post(self, request, *args, **kwargs):
+        return super().get(self, *args, *kwargs)
+
+    def get_queryset(self):
+        lib = library_to_view(self.request)
+        r = ArgsLibrary()
+        if self.request.method == "POST":
+            r = get_args_library(self.request)
+        books_cards_sorted = sort_books(r, library_books=lib.books_cards)
+        lib.books_cards = books_cards_sorted
+        info_sort_library(self.request, r)
+        return lib
 
     def get_context_data(self, *args, **kwargs):
         context = super().get_context_data(*args, **kwargs)
-        request = self.request
-        # library view sorting params
-        r = ArgsLibrary()
-        if request.method == "POST":
-            r = get_args_library(request)
-        lib = library_to_view(request)
-        # library view sorting
-        books_cards = sort_books(r, library_books=lib.books_cards)
-        info_sort_library(request, r)
 
-        context["books"] = books_cards
-        # data for dropdowns authors, categories:
-        context["authors"] = lib.all_authors
-        context["categories"] = lib.all_categories
-        # book cover image size:
+        # book image size: width, height
         context["width"] = 105
         context["height"] = 150
+
         return context
 
 
@@ -52,19 +50,22 @@ class BookDetailView(DetailView):
     model = DB_Book
     template_name = "library/library_book.html"
     book = Book()
+    library = Library()
 
     def get_object(self, queryset=None):
         self.book = DB_Book.objects.get(google_book_id=self.kwargs.get("slug"))
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
+
         book = book_prepare(self.book)
-        all_authors = json.loads(self.request.session.get("auth"))
-        all_categories = json.loads(self.request.session.get("cat-"))
+        self.library.all_authors = json.loads(self.request.session.get("auth"))
+        self.library.all_categories = json.loads(self.request.session.get("cat-"))
 
         context["book"] = book
-        context["authors"] = all_authors
-        context["categories"] = all_categories
+        context["library"] = self.library
+        # book image size: width, height
         context["width"] = 150
         context["height"] = 210
+
         return context
