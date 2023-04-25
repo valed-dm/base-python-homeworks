6f46d6601c16f4320353bf806412b74c987ed917
valed-dm <dmvaled@gmail.com>
2023-04-25 09:35:35 +0300

    book = Book() global object replaced by its instance

--- gbooks/api/api/fetch_prepare.py
+++ gbooks/api/api/fetch_prepare.py
@@ -13,26 +13,27 @@ def date_restore(date: str) -> str:
 
 
 def data_prepare(item):
-    Book.google_book_id = item['id']
+    book = Book()
+    book.google_book_id = item['id']
 
     if 'authors' in item['volumeInfo'] and item['volumeInfo']['authors']:
-        Book.authors = item['volumeInfo']['authors']
+        book.authors = item['volumeInfo']['authors']
 
     if 'categories' in item['volumeInfo'] and item['volumeInfo']['categories']:
-        Book.categories = item['volumeInfo']['categories']
+        book.categories = item['volumeInfo']['categories']
 
     if 'publishedDate' in item['volumeInfo'] and item['volumeInfo']['publishedDate']:
-        Book.date = date_restore(item['volumeInfo']['publishedDate'])
+        book.date = date_restore(item['volumeInfo']['publishedDate'])
 
     if 'description' in item['volumeInfo'] and item['volumeInfo']['description']:
-        Book.description = item['volumeInfo']['description']
+        book.description = item['volumeInfo']['description']
 
     if 'imageLinks' in item['volumeInfo'] and item['volumeInfo']['imageLinks']['thumbnail']:
-        Book.image_src = item['volumeInfo']['imageLinks']['thumbnail']
+        book.image_src = item['volumeInfo']['imageLinks']['thumbnail']
     elif 'imageLinks' in item['volumeInfo'] and item['volumeInfo']['imageLinks']['smallThumbnail']:
-        Book.image_src = item['volumeInfo']['imageLinks']['smallThumbnail']
+        book.image_src = item['volumeInfo']['imageLinks']['smallThumbnail']
 
     if item['volumeInfo']['title']:
-        Book.title = item['volumeInfo']['title']
+        book.title = item['volumeInfo']['title']
 
-    return Book
+    return book
