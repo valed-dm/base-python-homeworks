8e9fc42a770c95d5bbc26b5f9d0e20532522a4da
valed-dm <dmvaled@gmail.com>
2023-05-03 07:55:20 +0300

    get_book modified for context test

--- gbooks/api/helpers/get_book.py
+++ gbooks/api/helpers/get_book.py
@@ -1,9 +1,20 @@
 import json
 
+from api.schemas import Book
+
 
 def get_book(request):
-    b_key = request.GET.get('id')
-    b_serialized = request.session.get(f'{b_key}', None)
-    book = json.loads(b_serialized)
+    book = Book()
+    book.title = "Sample single book view (no book data available)"
+    book.description = "This sample can't be added to library"
 
-    return book
+    try:
+        if request.__dict__["session"].__dict__['_SessionBase__session_key']:
+            b_key = request.GET.get('id')
+            b_serialized = request.session.get(f'{b_key}', None)
+            book = json.loads(b_serialized)
+            return book
+    except TypeError:
+        pass
+
+    return book.__dict__
